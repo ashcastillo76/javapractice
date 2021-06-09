978d56d2eb6ce28cc5bcc4cbcbab24d8153cec42
ashcastillo76 <ashcastillo76@gmail.com>
2021-06-09 12:06:40 -0700

    practice with oop

---
+++ Human/Human.java
@@ -0,0 +1,24 @@
+public class Human{
+    public int strength; 
+    public int stealth;
+    public int intelligence; 
+    public int health; 
+
+    public Human(){
+        this.strength =3;
+        this.stealth =3;
+        this.intelligence=3;
+        this.health=100;
+    }
+
+    public Human(int strength){
+        this.strength =strength;
+    }
+
+
+    public void attack(Human target){
+
+        target.health -= this.strength;
+        System.out.println(target.health);
+    }
+}
---
+++ Human/HumanTest.java
@@ -0,0 +1,11 @@
+public class HumanTest {
+    public static void main(String[] args){
+        Human h1 = new Human();
+        Human h2 = new Human();
+        Human h3 = new Human();
+        h1.attack(h2);
+        h1.attack(h3);
+        System.out.println(h1.health);
+
+    }
+}
